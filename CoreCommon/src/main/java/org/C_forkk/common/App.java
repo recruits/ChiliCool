a0cf1b4a7d5c7a9ddbc84fb5479201f4a430813e
recruits <zhangzc.zzc@gmail.com>
2015-03-30 14:27:42 +0800

    新增加模板生成代码功能

--- CoreCommon/src/main/java/org/C_forkk/common/App.java
+++ CoreCommon/src/main/java/org/C_forkk/common/App.java
@@ -1,5 +1,9 @@
 package org.C_forkk.common;
 
+import java.text.MessageFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
@@ -7,7 +11,22 @@ package org.C_forkk.common;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {	
+    	String beanDefInfo = "public class {0} extends ITemplateBean \\\\{";
+    	System.out.println(MessageFormat.format(beanDefInfo, "TestNew"));
+    	
+    	/*
+    	// <dataSourceExpression><![CDATA[new net.sf.jasperreports.engine.data.JRBeanCollectionDataSource($F{items})]]></dataSourceExpression>
+    	String constStr = "<dataSourceExpression><![CDATA[new net.sf.jasperreports.engine.data.JRBeanCollectionDataSource($F{items})]]></dataSourceExpression>				<subreportExpression><![CDATA[$P{SUBREPORT_DIR} + \"GNNormalCargoItem.jasper\"]]></subreportExpression>";
+    	
+		Pattern attrReg = Pattern
+				.compile("<dataSourceExpression><!\\[CDATA\\[new net.sf.jasperreports.engine.data.JRBeanCollectionDataSource\\(\\$F\\{items}\\)\\]\\]></dataSourceExpression>\\s+<subreportExpression><!\\[CDATA\\[\\$P\\{SUBREPORT_DIR\\}\\s+\\+\\s+\"(\\w+).jasper\"\\]\\]></subreportExpression>");
+		Matcher attrMat = attrReg.matcher(constStr);
+		while (attrMat.find()) {
+			String attrName = attrMat.group(1);
+			System.out.println(attrName);
+		}
+    	*/
         System.out.println( "Hello World!" );
         
         System.out.println(System.getProperties());
